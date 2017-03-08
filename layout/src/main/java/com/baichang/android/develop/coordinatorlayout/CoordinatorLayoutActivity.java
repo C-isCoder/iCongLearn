18e730a0ccdef727f1c72c99bfc91947fca0811a
orzlllll@qq.com <congxiaodan>
2017-03-08 18:07:57 +0800

    update

--- layout/src/main/java/com/baichang/android/develop/coordinatorlayout/CoordinatorLayoutActivity.java
+++ layout/src/main/java/com/baichang/android/develop/coordinatorlayout/CoordinatorLayoutActivity.java
@@ -4,13 +4,14 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.RecyclerView.ViewHolder;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.ViewGroup;
 import com.baichang.android.develop.R;
-import java.util.ArrayList;
-import java.util.List;
 
 public class CoordinatorLayoutActivity extends AppCompatActivity {
 
@@ -20,7 +21,10 @@ public class CoordinatorLayoutActivity extends AppCompatActivity {
     setContentView(R.layout.activity_coordinator_layout);
     Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
     setSupportActionBar(toolbar);
-
+    RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+    recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+    recyclerView.setAdapter(new Adapter());
     FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
     fab.setOnClickListener(new View.OnClickListener() {
       @Override
@@ -29,5 +33,33 @@ public class CoordinatorLayoutActivity extends AppCompatActivity {
             .setAction("Action", null).show();
       }
     });
+  }
+
+
+  class Adapter extends RecyclerView.Adapter<Adapter.Holder> {
+
+
+    @Override
+    public Holder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+      return new Holder(getLayoutInflater().inflate(R.layout.item_layout, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(Holder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+      return 20;
+    }
+
+    class Holder extends ViewHolder {
+
+      public Holder(View itemView) {
+        super(itemView);
+      }
+    }
   }
 }
