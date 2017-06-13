96da334c9222f5d3753da5afdc70cae46ebaeb37
orzlllll@qq.com <congxiaodan>
2017-06-13 16:42:34 +0800

    update

--- layout/src/main/java/com/baichang/android/develop/recycleritemdecoration/RecyclerItemDecorationActivity.java
+++ layout/src/main/java/com/baichang/android/develop/recycleritemdecoration/RecyclerItemDecorationActivity.java
@@ -1,6 +1,9 @@
 package com.baichang.android.develop.recycleritemdecoration;
 
+import android.content.Context;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.graphics.Rect;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -31,17 +34,18 @@ public class RecyclerItemDecorationActivity extends AppCompatActivity {
 
       RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
       recyclerView.setAdapter(new Adapter());
-      recyclerView.addItemDecoration(new ItemDecoration() {
-         @Override
-         public void getItemOffsets(Rect outRect, View view, RecyclerView parent, State state) {
-            outRect.top = 1;
-         }
-      });
+//      recyclerView.addItemDecoration(new ItemDecoration() {
+//         @Override
+//         public void getItemOffsets(Rect outRect, View view, RecyclerView parent, State state) {
+//            outRect.top = 1;
+//         }
+//      });
+      recyclerView.addItemDecoration(new ColorItemDecoration());
    }
 
    private void initData() {
-      for (int i = 0; i < 10; i++) {
-         mList.add("Text" + i);
+      for (int i = 0; i < 100; i++) {
+         mList.add("Text" + (i + 1));
       }
    }
 
@@ -53,6 +57,7 @@ public class RecyclerItemDecorationActivity extends AppCompatActivity {
          TextView view = new TextView(parent.getContext());
          view.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
          view.setTextSize(14);
+         view.setPadding(20, 20, 20, 20);
          view.setBackgroundColor(Color.WHITE);
          view.setTextColor(Color.BLACK);
          return new Holder(view);
@@ -73,6 +78,74 @@ public class RecyclerItemDecorationActivity extends AppCompatActivity {
 
       Holder(View itemView) {
          super(itemView);
+      }
+   }
+
+   private class ColorItemDecoration extends ItemDecoration {
+
+      private float mDividerHeight;
+      private Paint mPaint;
+
+      @Override
+      public void getItemOffsets(Rect outRect, View view, RecyclerView parent, State state) {
+         super.getItemOffsets(outRect, view, parent, state);
+         // 第一个ItemView 不需要在上面绘制
+         if (parent.getChildAdapterPosition(view) != 0) {
+            outRect.top = 1;
+            mDividerHeight = 2;
+         }
+      }
+
+      @Override
+      public void onDraw(Canvas c, RecyclerView parent, State state) {
+         mPaint = new Paint();
+         mPaint.setColor(Color.RED);
+         mPaint.setAntiAlias(true);
+         int childCount = parent.getChildCount();
+         for (int i = 0; i < childCount; i++) {
+            View view = parent.getChildAt(i);
+            int index = parent.getChildAdapterPosition(view);
+            // 第一个ItemView 不需要绘制
+            if (index == 0) {
+               continue;
+            }
+            float dividerTop = view.getTop() - mDividerHeight;
+            float diverLeft = parent.getPaddingLeft();
+            float dividerBottom = view.getTop();
+            float dividerRight = parent.getWidth() - parent.getPaddingRight();
+            c.drawRect(diverLeft, dividerTop, dividerRight, dividerBottom, mPaint);
+         }
+      }
+   }
+
+   private class TimeLineItemDecoration extends ItemDecoration {
+
+      private Paint mPaint;
+      // ItemVIew 左边的间距
+      private float mOffseLeft;
+      // ItemView 右边的间距
+      private float mOffsetTop;
+      // 时间轴节点的半径
+      private float mNodeRadius;
+
+      public TimeLineItemDecoration(Context context) {
+         mPaint = new Paint();
+         mPaint.setAntiAlias(true);
+         mPaint.setColor(Color.RED);
+         // 获取ItemView 左右距离 也可以通过工具 dp2px
+         mOffseLeft = context.getResources().getDimension(R.dimen.timeline_item_offset_left);
+         mNodeRadius = context.getResources().getDimension(R.dimen.timeline_item_node_radius);
+      }
+
+      @Override
+      public void getItemOffsets(Rect outRect, View view, RecyclerView parent, State state) {
+         super.getItemOffsets(outRect, view, parent, state);
+         if (parent.getChildAdapterPosition(view)!=0){
+            // 硬编码1px
+            outRect.top = 1;
+            mOffsetTop = 1;
+         }
+         outRect.left = (int) mOffseLeft;
       }
    }
 }
